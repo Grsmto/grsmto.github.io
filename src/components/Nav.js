15ba326127b54a51be22965eda8e420e4d61ca0b
Adrien Grsmto <grsmto@gmail.com>
2017-10-02 00:20:43 +0100

    new attempt to fix ugly nav underline (but still bad...)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,29 +3,42 @@ import Link from 'gatsby-link';
 
 import styles from './Nav.module.css';
 
-const Nav = () =>
-  <nav className={styles.nav}>
-    <ul className="list-reset m0 right-align">
-      <li className="inline-block">
-        <Link
-          className={styles.link}
-          activeClassName={styles.active}
-          to="/"
-          exact
-        >
-          prjcts
-        </Link>
-      </li>
-      <li className="inline-block ml3">
-        <Link
-          className={styles.link}
-          activeClassName={styles.active}
-          to="/about"
-        >
-          abt
-        </Link>
-      </li>
-    </ul>
-  </nav>;
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      freq: 0
+    };
+  }
+  render() {
+    return (
+      <nav className={styles.nav}>
+        <ul className="list-reset m0 right-align">
+          <li className="inline-block">
+            <Link
+              className={styles.link}
+              activeClassName={styles.active}
+              to="/"
+              exact
+              data-text="prjcts"
+            >
+              prjcts
+            </Link>
+          </li>
+          <li className="inline-block ml3">
+            <Link
+              className={styles.link}
+              activeClassName={styles.active}
+              to="/about"
+              data-text="abt"
+            >
+              abt
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default Nav;
