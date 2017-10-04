d2fc9b200f39bf0ee6bab412487e336b323d1420
Adrien Grsmto <grsmto@gmail.com>
2017-10-04 03:00:58 +0100

    add social icons

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,8 @@ import mutateStore from '../state/mutateStore';
 
 import styles from '../layouts/about.module.css';
 
+import { IconTwitter, IconGithub, IconCodepen, IconLinkedIn } from '../components/Icons';
+
 import bgJpg from '../assets/images/me.jpg';
 import bgWebP from '../assets/images/me.webp';
 
@@ -18,7 +20,7 @@ class About extends React.Component {
 
   render() {
     return (
-      <div className={styles.about}>
+      <div className={`page ${styles.about}`}>
         <Helmet title="Adrien Denat | About" />
         <div className={styles.container}>
           <div className={styles.inner}>
@@ -29,12 +31,19 @@ class About extends React.Component {
               freelance dev based in London,<br />
               currently working from Brazil.
             </h2>
-            <p>
+            <p className={styles.shortBio}>
               I love building websites and products with a meaning. <br />
-              I like sharing about frontend development on my Twitter and
-              sometimes publish articles.
+              Ready to work on a wide type of project but I specialise in software engineering.<br />
+              I like sharing about frontend development on my <a href="https://twitter.com/Grsmto" target="_blank" rel="noopener noreferrer">Twitter</a> and writting longer thoughts
+              on <a href="https://tympanus.net/codrops/2016/05/11/distorted-button-effects-with-svg-filters/" target="blank" rel="noopener noreferrer">articles</a>.
             </p>
-            <a href="mailto:oi@adriendenat.com">oi@adriendenat.com</a>
+            <a className={styles.email} href="mailto:oi@adriendenat.com">oi@adriendenat.com</a>
+            <div className={styles.socialButtons}>
+              <a className={styles.socialButton} href="https://github.com/Grsmto/" target="_blank" rel="noopener noreferrer"><IconGithub /></a>
+              <a className={styles.socialButton} href="https://codepen.io/Grsmto/" target="_blank" rel="noopener noreferrer"><IconCodepen /></a>
+              <a className={styles.socialButton} href="https://twitter.com/Grsmto" target="_blank" rel="noopener noreferrer"><IconTwitter /></a>
+              <a className={styles.socialButton} href="https://www.linkedin.com/in/adriendenat/" target="_blank" rel="noopener noreferrer"><IconLinkedIn /></a>
+            </div>
           </div>
         </div>
         <picture className={styles.bg}>
