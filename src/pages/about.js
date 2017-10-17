e4b6f09c9e8fd450970703c94d6e8ec26fc196f3
Adrien Grsmto <grsmto@gmail.com>
2017-10-17 00:20:05 -0200

    new about page style

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
+import classnames from 'classnames';
 
 import mutateStore from '../state/mutateStore';
 
 import styles from '../layouts/about.module.css';
-
-import {
-  IconTwitter,
-  IconGithub,
-  IconCodepen,
-  IconLinkedIn
-} from '../components/Icons';
 
 import bgJpg from '../assets/images/me.jpg';
 import bgWebP from '../assets/images/me.webp';
@@ -27,20 +21,73 @@ class About extends React.Component {
     return (
       <div className={`page ${styles.about}`}>
         <Helmet title="Adrien Denat | About" />
+        <div className={styles.hero}>
+          <div className={styles.heroInner}>
+            <h1 className={styles.title}>
+              <span>Oi!</span><br/>
+              <span>I’M</span><br/>
+              <span>DEV!</span>
+            </h1>
+          </div>
+          <picture className={styles.bg}>
+            <source srcSet={bgWebP} type="image/webp" />
+            <source srcSet={bgJpg} type="image/jpeg" />
+            <img className={styles.bgImg} src={bgJpg} alt="Me" />
+          </picture>
+        </div>
         <div className={styles.container}>
           <div className={styles.inner}>
-            <h1 className={styles.title}>
-              Oi! <br className={styles.titleBr} /> I’m Adrien,
-            </h1>
+            <ul className={classnames('list-reset', styles.socialLinks)}>
+              <li>
+                <a
+                  className={styles.socialLink}
+                  href="https://github.com/Grsmto/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.socialLink}
+                  href="https://codepen.io/Grsmto/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                Codepen
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.socialLink}
+                  href="https://twitter.com/Grsmto"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                Twitter
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.socialLink}
+                  href="https://www.linkedin.com/in/adriendenat/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                LinkedIn
+                </a>
+              </li>
+            </ul>
             <h2 className={styles.description}>
-              freelance dev based in London,<br />
+              Freelance dev based in London,<br />
               currently working from Brazil.
             </h2>
             <p className={styles.shortBio}>
               I love building websites and products with a meaning. <br />
-              Ready to work on a wide type of project but I specialise in
-              software engineering.<br />
-              I like sharing about frontend development on my{' '}
+              Beautiful interfaces is my thing.<br />
+              I specialise in software engineering but I'm ready to work on a wide variety of project.<br />
+              I like to share about frontend development on my{' '}
               <a
                 href="https://twitter.com/Grsmto"
                 target="_blank"
@@ -48,7 +95,7 @@ class About extends React.Component {
               >
                 Twitter
               </a>{' '}
-              and writting longer thoughts on{' '}
+              and sometimes write longer thoughts on{' '}
               <a
                 href="https://tympanus.net/codrops/2016/05/11/distorted-button-effects-with-svg-filters/"
                 target="blank"
@@ -57,50 +104,24 @@ class About extends React.Component {
                 articles
               </a>.
             </p>
-            <a className={styles.email} href="mailto:oi@adriendenat.com">
-              oi@adriendenat.com
-            </a>
-            <div className={styles.socialButtons}>
-              <a
-                className={styles.socialButton}
-                href="https://github.com/Grsmto/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <IconGithub />
+            <span className={styles.email}>
+              Email me:{' '}
+              <a href="mailto:oi@adriendenat.com">
+                oi@adriendenat.com
               </a>
-              <a
-                className={styles.socialButton}
-                href="https://codepen.io/Grsmto/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <IconCodepen />
-              </a>
-              <a
-                className={styles.socialButton}
-                href="https://twitter.com/Grsmto"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <IconTwitter />
-              </a>
-              <a
-                className={styles.socialButton}
-                href="https://www.linkedin.com/in/adriendenat/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <IconLinkedIn />
-              </a>
+            </span>
+            <div className={styles.grid}>
+              <p className={styles.bio}>
+                Within 7 years of experience in frontend development, I've been working on <a href="http://thefwa.com/cases/creaktif" target="_blank" rel="noopener noreferrer">awarded</a> marketing websites, products and mobile applications.
+                I recently left my position of lead frontend developer at NEVERBLAND to start working as a freelancer.<br/>
+                I now have about 5 projects in production using React, Redux and also GraphQL, but I'm open to other frameworks too!
+                I'm always looking for the fastest, cleanest and most efficient way to build user interfaces.
+                Recently I have been focusing on mobile applications using React Native and Java on Android.<br />
+                I love remote work and I'll make you change your mind about how efficient it can be!
+              </p>
             </div>
           </div>
         </div>
-        <picture className={styles.bg}>
-          <source srcSet={bgWebP} type="image/webp" />
-          <source srcSet={bgJpg} type="image/jpeg" />
-          <img className={styles.bgImg} src={bgJpg} alt="Me" />
-        </picture>
       </div>
     );
   }
