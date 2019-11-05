710683b79f3d21de202ae96e18f3432ee0c0baa9
Adrien Denat <adrien.denat@gmail.com>
2019-11-05 15:30:21 -0300

    feat: add react-headroom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,57 @@
 import React from "react";
 import { Link } from "gatsby";
-import { Flex } from "rebass";
+import { Flex } from "@theme-ui/components";
+import Headroom from "react-headroom";
 
 import Container from "./Container";
 import Nav from "./Nav";
 import Logo from "./Logo";
 
 const Header = ({ isIntroDone, sx }) => (
-  <Container
-    as="header"
-    sx={{
-      position: "fixed",
-      zIndex: "header",
-      width: "100%",
-      opacity: 0,
-      visibility: "hidden",
-      transition: "all 300ms",
-      top: 0,
-      left: 0,
-      right: 0,
+  <Headroom>
+    <Container
+      as="header"
+      sx={{
+        // position: "fixed",
+        zIndex: "header",
+        width: "100%",
+        opacity: 0,
+        visibility: "hidden",
+        transition: "all 300ms",
+        top: 0,
+        left: 0,
+        right: 0,
 
-      ...(isIntroDone && {
-        opacity: 1,
-        visibility: "visible",
-      }),
+        ...(isIntroDone && {
+          opacity: 1,
+          visibility: "visible",
+        }),
 
-      "&:before": {
-        position: "absolute",
-        content: '"Boing!"',
-        top: "-3rem",
-        left: "50%",
-        transform: "translateX(-50 %)",
-      },
+        "&:before": {
+          position: "absolute",
+          content: '"Boing!"',
+          top: "-3rem",
+          left: "50%",
+          transform: "translateX(-50 %)",
+        },
 
-      ...sx,
-    }}
-  >
-    <Flex>
-      {isIntroDone && (
-        <Link to="/">
-          <Logo
-            sx={{
-              display: "inline-block",
-            }}
-          />
-        </Link>
-      )}
-      <Nav sx={{ ml: "auto" }} />
-    </Flex>
-  </Container>
+        ...sx,
+      }}
+    >
+      <Flex>
+        {isIntroDone && (
+          <Link to="/">
+            <Logo
+              sx={{
+                display: "inline-block",
+              }}
+            />
+          </Link>
+        )}
+        <Nav sx={{ ml: "auto" }} />
+      </Flex>
+    </Container>
+  </Headroom>
 );
 
 export default Header;
